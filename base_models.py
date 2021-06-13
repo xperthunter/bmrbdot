7f095dda6f1a4fb1cbf84ef48d70a77f62ceb852
xperthunter <kjfraga@ucdavis.ed>
2021-06-13 01:06:44 -0700

    base models update. all atoms predicted

--- base_models.py
+++ base_models.py
@@ -11,7 +11,9 @@ import random
 from sklearn.model_selection import KFold
 import sys
 
+from pytablewriter import MarkdownTableWriter
 import sequence_filter
+
 
 parser = argparse.ArgumentParser(description=''.join(('Construct base models',
 	' for chemical shift prediction. Base models are shift averages constructed',
@@ -78,9 +80,8 @@ def make_set(df, kill):
 
 arg = parser.parse_args()
 
-dfin = pd.read_json(arg.data,compression='xz').reset_index()
+dfin = pd.read_json(arg.data,compression='xz').sample(frac=1.0).reset_index()
 print(f'Input shape: {dfin.shape}')
-
 kill = {}
 if arg.skips:
 	with open(arg.skips) as fp:
@@ -92,237 +93,460 @@ if arg.blast:
 	assert(arg.identity < 101)
 	
 	keep = sequence_filter.identity_filter(arg.blast, arg.identity)
+	keep = [int(k) for k in keep]
 	df = dfin[dfin['name'].isin(keep)]
 	print(f'After filtering: {df.shape}')
 	heldout_df = dfin[~dfin['name'].isin(keep)]
-	
+
 	held_elms, held_set = make_set(heldout_df, kill)
 
 elms, sets = make_set(df, kill)
 
-print(f'{"model":<7} {"Nrmse":>7} {"Nout":>7} {"Hrmse":>7} {"Hout":>7}',end='')
-print(f' {"Crmse":>7} {"Cout":>7} {"Sumrmse":>7} {"Sumout":>7}')
+print(df.columns)
+headers = ['model']
+atmpos = []
+for el in sets:
+	for s in sets[el]:
+		if s['atm'] not in headers:
+			headers.append(s['atm'])
+			atmpos.append(s['atm'])
 
-print(f'{"element":<7}', end=' ')
-sumin  = 0
-sumout = 0
-for el in elms.keys():
-	shifts = np.array(elms[el])
-	shifts = shifts[np.where(shifts != None)]
-	rmse = performance(shifts)
-	sumin += rmse
-	print(f'{rmse:{" "}>7.4f}', end=' ')
-	
-	held_rmse = 0
-	tot = np.mean(shifts)
-	hs = np.array(held_elms[el])
-	hs = hs[np.where(hs != None)]
-	for t in hs: 
-		held_rmse += (t - tot)**2
-	held_rmse /= hs.shape[0]
-	held_rmse = sqrt(held_rmse)
-	print(f'{held_rmse:{" "}>7.4f}', end=' ')
-	sumout += held_rmse
+print(headers)
+print(atmpos)
+performances = []
 
-print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+all_sets = []
+for el in sets:
+	all_sets.extend(sets[el])
 
-print(f'{"atom":<7}', end=' ')
-sumin  = 0
-sumout = 0
-for el in elms.keys():
-	shifts = sets[el]
-	random.shuffle(shifts)
+random.shuffle(all_sets)
+
+kf = KFold(n_splits=10)
+kf.get_n_splits(all_sets)
+val_rmse = 0
+
+train = []
+test  = []
+perf  = {}
+pres  = {}
+for aa in atmpos: 
+	perf[aa] = 0
+	pres[aa] = 0
+
+for tr, te in kf.split(all_sets):
+	train = [all_sets[i] for i in tr]
+	test  = [all_sets[i] for i in te]
+	model = dict()
 	
-	kf = KFold(n_splits=10)
-	kf.get_n_splits(shifts)
-	val_rmse = 0
+	for s in train:
+		if s['elm'] not in model: model[s['elm']] = list()
+		model[s['elm']].append(s['cs'])
 	
-	train = []
-	test  = []
+	for k in model.keys():
+		cs = np.array(model[k])
+		model[k] = np.mean(cs)
 	
-	for tr, te in kf.split(shifts):
-		train = [shifts[i] for i in tr]
-		test  = [shifts[i] for i in te]
-		model = dict()
-		
-		for s in train:
-			if s['atm'] not in model: model[s['atm']] = list()
-			model[s['atm']].append(s['cs'])
-		
-		for k in model.keys():
-			cs = np.array(model[k])
-			model[k] = np.mean(cs)
-		
+	rmse = 0
+	for aa in perf:
 		rmse = 0
+		count = 0
 		for ts in test:
+			if ts['atm'] != aa: continue 
+			count += 1
+			rmse += (ts['cs'] - model[ts['elm']])**2
+		
+		if count == 0: continue
+		pres[aa] += 1
+		rmse /= count
+		rmse = sqrt(rmse)
+		perf[aa] += rmse
+
+for aa in perf: perf[aa] /= pres[aa]
+
+elmperf = ['element']
+elmperf.extend(list(perf.values()))
+performances.append(elmperf)
+
+train = []
+test  = []
+perf  = {}
+pres  = {}
+for aa in atmpos: 
+	perf[aa] = 0
+	pres[aa] = 0
+
+for tr, te in kf.split(all_sets):
+	train = [all_sets[i] for i in tr]
+	test  = [all_sets[i] for i in te]
+	model = dict()
+	
+	for s in train:
+		if s['atm'] not in model: model[s['atm']] = list()
+		model[s['atm']].append(s['cs'])
+	
+	for k in model.keys():
+		cs = np.array(model[k])
+		model[k] = np.mean(cs)
+	
+	rmse = 0
+	for aa in perf:
+		if aa not in model: continue
+		rmse = 0
+		count = 0
+		for ts in test:
+			if ts['atm'] != aa: continue 
+			count += 1
 			rmse += (ts['cs'] - model[ts['atm']])**2
 		
-		rmse /= len(test)
+		if count == 0: continue
+		pres[aa] += 1
+		rmse /= count
 		rmse = sqrt(rmse)
-		#print(rmse)
-		val_rmse += rmse
-	val_rmse /= 10
-	sumin += val_rmse
-	print(f'{val_rmse:{" "}>7.4f}', end=' ')
-	
-	overall = dict()
-	for si in shifts:
-		if si['atm'] not in overall: overall[si['atm']] = list()
-		overall[si['atm']].append(si['cs'])
-	
-	for k in overall.keys():
-		val = np.mean(np.array(overall[k]))
-		overall[k] = val
-	
-	outrmse = 0
-	for sh in held_set[el]:
-		outrmse += (sh['cs'] - overall[sh['atm']])**2
-	
-	outrmse /= len(held_set[el])
-	outrmse = sqrt(outrmse)
-	print(f'{outrmse:{" "}>7.4f}', end=' ')
-	sumout += outrmse
+		perf[aa] += rmse
 
-print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+for aa in perf: 
+	if pres[aa] == 0: continue
+	perf[aa] /= pres[aa]
 
-print(f'{"atm-res":<7}', end=' ')
-sumin  = 0
-sumout = 0
-for el in elms.keys():
-	shifts = sets[el]
-	random.shuffle(shifts)
+atmperf = ['atom']
+atmperf.extend(list(perf.values()))
+performances.append(atmperf)
+
+train = []
+test  = []
+perf  = {}
+pres  = {}
+for aa in atmpos: 
+	perf[aa] = 0
+	pres[aa] = 0
+
+for tr, te in kf.split(all_sets):
+	train = [all_sets[i] for i in tr]
+	test  = [all_sets[i] for i in te]
+	model = dict()
 	
-	kf = KFold(n_splits=10)
-	kf.get_n_splits(shifts)
-	val_rmse = 0
+	for s in train:
+		if s['res'] not in model: model[s['res']] = dict()
+		if s['atm'] not in model[s['res']]: model[s['res']][s['atm']] = []
+		model[s['res']][s['atm']].append(s['cs'])
 	
-	train = []
-	test  = []
+	for k1 in model.keys():
+		for k2 in model[k1].keys():
+			cs = np.array(model[k1][k2])
+			model[k1][k2] = np.mean(cs)
 	
-	for tr, te in kf.split(shifts):
-		train = [shifts[i] for i in tr]
-		test  = [shifts[i] for i in te]
-		model = dict()
-		
-		for s in train:
-			if s['res'] not in model: model[s['res']] = dict()
-			if s['atm'] not in model[s['res']]: model[s['res']][s['atm']] = []
-			model[s['res']][s['atm']].append(s['cs'])
-		
-		for k1 in model.keys():
-			for k2 in model[k1].keys():
-				cs = np.array(model[k1][k2])
-				model[k1][k2] = np.mean(cs)
-		
+	rmse = 0
+	for aa in perf:
 		rmse = 0
+		count = 0
 		for ts in test:
+			if ts['atm'] != aa: continue
+			if ts['res'] not in model: continue
+			if ts['atm'] not in model[ts['res']]: continue
+			count += 1
 			rmse += (ts['cs'] - model[ts['res']][ts['atm']])**2
 		
-		rmse /= len(test)
+		if count == 0: continue
+		pres[aa] += 1
+		rmse /= count
 		rmse = sqrt(rmse)
-		#print(rmse)
-		val_rmse += rmse
-	val_rmse /= 10
-	sumin += val_rmse
-	print(f'{val_rmse:{" "}>7.4f}', end=' ')
-	
-	overall = dict()
-	for s in shifts:
-		if s['res'] not in overall: overall[s['res']] = dict()
-		if s['atm'] not in overall[s['res']]: overall[s['res']][s['atm']] = []
-		overall[s['res']][s['atm']].append(s['cs'])
-	
-	for k1 in overall.keys():
-		for k2 in overall[k1].keys():
-			val = np.mean(np.array(overall[k1][k2]))
-			overall[k1][k2] = val
-	
-	outrmse = 0
-	for s in held_set[el]:
-		outrmse += (s['cs'] - overall[s['res']][s['atm']])**2
-	
-	outrmse /= len(held_set[el])
-	outrmse = sqrt(outrmse)
-	print(f'{outrmse:{" "}>7.4f}', end=' ')
-	sumout += outrmse
+		perf[aa] += rmse
 
-print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+for aa in perf: 
+	if pres[aa] == 0: continue
+	perf[aa] /= pres[aa]
 
-print(f'{"3meratm":<7}', end=' ')
-sumin  = 0
-sumout = 0
-for el in elms.keys():
-	shifts = sets[el]
-	random.shuffle(shifts)
+atmresperf = ['atm-res']
+atmresperf.extend(list(perf.values()))
+performances.append(atmresperf)
+
+train = []
+test  = []
+perf  = {}
+pres  = {}
+for aa in atmpos: 
+	perf[aa] = 0
+	pres[aa] = 0
+
+for tr, te in kf.split(all_sets):
+	train = [all_sets[i] for i in tr]
+	test  = [all_sets[i] for i in te]
+	model = dict()
 	
-	kf = KFold(n_splits=10)
-	kf.get_n_splits(shifts)
-	val_rmse = 0
-	
-	train = []
-	test  = []
-	
-	for tr, te in kf.split(shifts):
-		train = [shifts[i] for i in tr]
-		test  = [shifts[i] for i in te]
-		model = dict()
-		
-		for s in train:
-			if s['resl'] == None or s['resr'] == None: continue
-			mer = s['resl']+s['res']+s['resr']
-			if mer not in model: model[mer] = dict()
-			if s['atm'] not in model[mer]: model[mer][s['atm']] = []
-			model[mer][s['atm']].append(s['cs'])
-		
-		for k1 in model.keys():
-			for k2 in model[k1].keys():
-				val = np.mean(np.array(model[k1][k2]))
-				model[k1][k2] = val
-		
-		rmse = 0
-		n = 0
-		for ts in test:
-			if ts['resl'] == None or ts['resr'] == None: continue
-			key = ts['resl']+ts['res']+ts['resr']
-			if key in model:
-				if ts['atm'] in model[key]:
-					rmse += (ts['cs'] - model[key][ts['atm']])**2
-					n += 1
-		
-		rmse /= n
-		rmse = sqrt(rmse)
-		#print(rmse)
-		val_rmse += rmse
-	val_rmse /= 10
-	sumin += val_rmse
-	print(f'{val_rmse:{" "}>7.4f}', end=' ')
-	
-	overall = dict()
-	for s in shifts:
+	for s in train:
 		if s['resl'] == None or s['resr'] == None: continue
 		mer = s['resl']+s['res']+s['resr']
-		if mer not in overall: overall[mer] = dict()
-		if s['atm'] not in overall[mer]: overall[mer][s['atm']] = []
-		overall[mer][s['atm']].append(s['cs'])
-	
-	for k1 in overall.keys():
-		for k2 in overall[k1].keys():
-			val = np.mean(np.array(overall[k1][k2]))
-			overall[k1][k2] = val
-	
-	outrmse = 0
-	n = 0
-	for s in held_set[el]:
-		if s['resl'] == None or s['resr'] == None: continue
-		merk = s['resl']+s['res']+s['resr']		
-		if merk in overall:
-			if s['atm'] in overall[merk]:
-				outrmse += (s['cs'] - overall[merk][s['atm']])**2
-				n += 1
-	
-	outrmse /= n
-	outrmse = sqrt(outrmse)
-	print(f'{outrmse:{" "}>7.4f}', end=' ')
-	sumout += outrmse
+		if mer not in model: model[mer] = dict()
+		if s['atm'] not in model[mer]: model[mer][s['atm']] = []
+		model[mer][s['atm']].append(s['cs'])
+		
+	for k1 in model.keys():
+		for k2 in model[k1].keys():
+			val = np.mean(np.array(model[k1][k2]))
+			model[k1][k2] = val
+		
+	rmse = 0
+	for aa in perf:
+		rmse = 0
+		count = 0
+		for ts in test:
+			if ts['atm'] != aa: continue
+			if ts['resl'] == None or ts['resr'] == None: continue
+			key = ts['resl']+ts['res']+ts['resr']
+			if key not in model: continue
+			if ts['atm'] not in model[key]: continue
+			count += 1
+			rmse += (ts['cs'] - model[key][ts['atm']])**2
+		
+		if count == 0: continue
+		pres[aa] += 1
+		rmse /= count
+		rmse = sqrt(rmse)
+		perf[aa] += rmse 
 
-print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+for aa in perf: 
+	if pres[aa] == 0: continue
+	perf[aa] /= pres[aa]
+
+merperf = ['3mer-atm']
+merperf.extend(list(perf.values()))
+performances.append(merperf)
+
+writer = MarkdownTableWriter(
+	table_name="Look-Up Table Model Results",
+	headers=headers,
+	value_matrix=performances,
+	margin=1
+)
+
+writer.write_table()
+
+
+# print(f'{"model":<7} {"Nrmse":>7} {"Nout":>7} {"Hrmse":>7} {"Hout":>7}',end='')
+# print(f' {"Crmse":>7} {"Cout":>7} {"Sumrmse":>7} {"Sumout":>7}')
+# 
+# print(f'{"element":<7}', end=' ')
+# sumin  = 0
+# sumout = 0
+# for el in elms.keys():
+# 	shifts = np.array(elms[el])
+# 	shifts = shifts[np.where(shifts != None)]
+# 	
+# 	
+# 	rmse = performance(shifts)
+# 	sumin += rmse
+# 	print(f'{rmse:{" "}>7.4f}', end=' ')
+# 	
+# 	held_rmse = 0
+# 	tot = np.mean(shifts)
+# 	hs = np.array(held_elms[el])
+# 	hs = hs[np.where(hs != None)]
+# 	for t in hs: 
+# 		held_rmse += (t - tot)**2
+# 	held_rmse /= hs.shape[0]
+# 	held_rmse = sqrt(held_rmse)
+# 	print(f'{held_rmse:{" "}>7.4f}', end=' ')
+# 	sumout += held_rmse
+# 
+# print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+# 
+# print(f'{"atom":<7}', end=' ')
+# sumin  = 0
+# sumout = 0
+# for el in elms.keys():
+# 	shifts = sets[el]
+# 	random.shuffle(shifts)
+# 	
+# 	kf = KFold(n_splits=10)
+# 	kf.get_n_splits(shifts)
+# 	val_rmse = 0
+# 	
+# 	train = []
+# 	test  = []
+# 	
+# 	for tr, te in kf.split(shifts):
+# 		train = [shifts[i] for i in tr]
+# 		test  = [shifts[i] for i in te]
+# 		model = dict()
+# 		
+# 		for s in train:
+# 			if s['atm'] not in model: model[s['atm']] = list()
+# 			model[s['atm']].append(s['cs'])
+# 		
+# 		for k in model.keys():
+# 			cs = np.array(model[k])
+# 			model[k] = np.mean(cs)
+# 		
+# 		rmse = 0
+# 		for ts in test:
+# 			rmse += (ts['cs'] - model[ts['atm']])**2
+# 		
+# 		rmse /= len(test)
+# 		rmse = sqrt(rmse)
+# 		#print(rmse)
+# 		val_rmse += rmse
+# 	val_rmse /= 10
+# 	sumin += val_rmse
+# 	print(f'{val_rmse:{" "}>7.4f}', end=' ')
+# 	
+# 	overall = dict()
+# 	for si in shifts:
+# 		if si['atm'] not in overall: overall[si['atm']] = list()
+# 		overall[si['atm']].append(si['cs'])
+# 	
+# 	for k in overall.keys():
+# 		val = np.mean(np.array(overall[k]))
+# 		overall[k] = val
+# 	
+# 	outrmse = 0
+# 	for sh in held_set[el]:
+# 		outrmse += (sh['cs'] - overall[sh['atm']])**2
+# 	
+# 	outrmse /= len(held_set[el])
+# 	outrmse = sqrt(outrmse)
+# 	print(f'{outrmse:{" "}>7.4f}', end=' ')
+# 	sumout += outrmse
+# 
+# print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+# 
+# print(f'{"atm-res":<7}', end=' ')
+# sumin  = 0
+# sumout = 0
+# for el in elms.keys():
+# 	shifts = sets[el]
+# 	random.shuffle(shifts)
+# 	
+# 	kf = KFold(n_splits=10)
+# 	kf.get_n_splits(shifts)
+# 	val_rmse = 0
+# 	
+# 	train = []
+# 	test  = []
+# 	
+# 	for tr, te in kf.split(shifts):
+# 		train = [shifts[i] for i in tr]
+# 		test  = [shifts[i] for i in te]
+# 		model = dict()
+# 		
+# 		for s in train:
+# 			if s['res'] not in model: model[s['res']] = dict()
+# 			if s['atm'] not in model[s['res']]: model[s['res']][s['atm']] = []
+# 			model[s['res']][s['atm']].append(s['cs'])
+# 		
+# 		for k1 in model.keys():
+# 			for k2 in model[k1].keys():
+# 				cs = np.array(model[k1][k2])
+# 				model[k1][k2] = np.mean(cs)
+# 		
+# 		rmse = 0
+# 		for ts in test:
+# 			rmse += (ts['cs'] - model[ts['res']][ts['atm']])**2
+# 		
+# 		rmse /= len(test)
+# 		rmse = sqrt(rmse)
+# 		#print(rmse)
+# 		val_rmse += rmse
+# 	val_rmse /= 10
+# 	sumin += val_rmse
+# 	print(f'{val_rmse:{" "}>7.4f}', end=' ')
+# 	
+# 	overall = dict()
+# 	for s in shifts:
+# 		if s['res'] not in overall: overall[s['res']] = dict()
+# 		if s['atm'] not in overall[s['res']]: overall[s['res']][s['atm']] = []
+# 		overall[s['res']][s['atm']].append(s['cs'])
+# 	
+# 	for k1 in overall.keys():
+# 		for k2 in overall[k1].keys():
+# 			val = np.mean(np.array(overall[k1][k2]))
+# 			overall[k1][k2] = val
+# 	
+# 	outrmse = 0
+# 	for s in held_set[el]:
+# 		outrmse += (s['cs'] - overall[s['res']][s['atm']])**2
+# 	
+# 	outrmse /= len(held_set[el])
+# 	outrmse = sqrt(outrmse)
+# 	print(f'{outrmse:{" "}>7.4f}', end=' ')
+# 	sumout += outrmse
+# 
+# print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
+# 
+# print(f'{"3meratm":<7}', end=' ')
+# sumin  = 0
+# sumout = 0
+# for el in elms.keys():
+# 	shifts = sets[el]
+# 	random.shuffle(shifts)
+# 	
+# 	kf = KFold(n_splits=10)
+# 	kf.get_n_splits(shifts)
+# 	val_rmse = 0
+# 	
+# 	train = []
+# 	test  = []
+# 	
+# 	for tr, te in kf.split(shifts):
+# 		train = [shifts[i] for i in tr]
+# 		test  = [shifts[i] for i in te]
+# 		model = dict()
+# 		
+# 		for s in train:
+# 			if s['resl'] == None or s['resr'] == None: continue
+# 			mer = s['resl']+s['res']+s['resr']
+# 			if mer not in model: model[mer] = dict()
+# 			if s['atm'] not in model[mer]: model[mer][s['atm']] = []
+# 			model[mer][s['atm']].append(s['cs'])
+# 		
+# 		for k1 in model.keys():
+# 			for k2 in model[k1].keys():
+# 				val = np.mean(np.array(model[k1][k2]))
+# 				model[k1][k2] = val
+# 		
+# 		rmse = 0
+# 		n = 0
+# 		for ts in test:
+# 			if ts['resl'] == None or ts['resr'] == None: continue
+# 			key = ts['resl']+ts['res']+ts['resr']
+# 			if key in model:
+# 				if ts['atm'] in model[key]:
+# 					rmse += (ts['cs'] - model[key][ts['atm']])**2
+# 					n += 1
+# 		
+# 		rmse /= n
+# 		rmse = sqrt(rmse)
+# 		#print(rmse)
+# 		val_rmse += rmse
+# 	val_rmse /= 10
+# 	sumin += val_rmse
+# 	print(f'{val_rmse:{" "}>7.4f}', end=' ')
+# 	
+# 	overall = dict()
+# 	for s in shifts:
+# 		if s['resl'] == None or s['resr'] == None: continue
+# 		mer = s['resl']+s['res']+s['resr']
+# 		if mer not in overall: overall[mer] = dict()
+# 		if s['atm'] not in overall[mer]: overall[mer][s['atm']] = []
+# 		overall[mer][s['atm']].append(s['cs'])
+# 	
+# 	for k1 in overall.keys():
+# 		for k2 in overall[k1].keys():
+# 			val = np.mean(np.array(overall[k1][k2]))
+# 			overall[k1][k2] = val
+# 	
+# 	outrmse = 0
+# 	n = 0
+# 	for s in held_set[el]:
+# 		if s['resl'] == None or s['resr'] == None: continue
+# 		merk = s['resl']+s['res']+s['resr']		
+# 		if merk in overall:
+# 			if s['atm'] in overall[merk]:
+# 				outrmse += (s['cs'] - overall[merk][s['atm']])**2
+# 				n += 1
+# 	
+# 	outrmse /= n
+# 	outrmse = sqrt(outrmse)
+# 	print(f'{outrmse:{" "}>7.4f}', end=' ')
+# 	sumout += outrmse
+# 
+# print(f'{sumin:{" "}>7.4f} {sumout:{" "}>7.4f}')
